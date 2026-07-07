390c1e3f59e31c5fa613217d5c1b3cb65133b8bf
Mahliyo <gasandigital13@gmail.com>
2026-07-07 19:05:52 +0500

    NextJs star

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@ import Link from 'next/link'
 import React from 'react'
 const product = [
   {id: '1', name: "Samsung Galaxy S21"},
-  {id: '1', name: "Samsung Galaxy S21"},
+  {id: '2', name: "Samsung Galaxy S21"},
 ]
 function HomePage() {
   return (
