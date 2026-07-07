e324eacea2326be73cde8e1dcce4c9f8ae79c40e
Mahliyo <gasandigital13@gmail.com>
2026-07-07 17:35:33 +0500

    NextJs star

---
+++ src/app/about/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function AboutPages() {
+  return (
+    <div>
+        <h1>About sahifa</h1>
+        <p>Bu about sahifasi</p>
+    </div>
+  )
+}
+
+export default AboutPages
--- src/app/layout.js
+++ src/app/layout.js
@@ -22,7 +22,10 @@ export default function RootLayout({ children }) {
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <header>Header</header>
+      <body className="min-h-full flex flex-col">{children}
+        <footer>Footer</footer>
+      </body>
     </html>
   );
 }
